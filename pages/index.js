88bb62bfacdb6aa5d6e4901da42515ab269c1ff4
HoDienCong12c5 <hodiecong12c5@gmail.com>
2022-09-29 17:30:35 +0700

    fix-seo

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
 
 import styles from '../styles/Home.module.css'
 import HomePage from 'pages/Screen/HomeScreen'
-import { NextSeo } from 'next-seo';
 export default function Home (props) {
   return (
     <div className={styles.container}>
-      <NextSeo
-        title="Mlem Coffee"
-        description="Cung cấp cà phê tốt nhất cho bạn."
-      />
       <HomePage /> 
     </div>
   )
